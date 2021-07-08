b11dd8da1bc8547cb9475efd4e2306327967dd6d
Karolina <karolina.urbaniak@akra.net>
2021-07-08 15:46:15 +0200

    map

---
+++ JS files/contact.js
@@ -0,0 +1,10 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+    });
+    const marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+    });
+}
